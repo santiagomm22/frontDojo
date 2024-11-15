c537d4dc1f3becf13b2a35bb7fa7a5373dc4832a
mscorrea17 <trabajosmichaelcorrea@gmail.com>
2024-11-15 11:11:33 -0500

    Actualizacion en el formulario  y tabla de usuarios creados

--- src/usuarios/Usuarios.tsx
+++ src/usuarios/Usuarios.tsx
@@ -3,23 +3,40 @@ import Form from "react-bootstrap/Form";
 
 export const Usuarios = () => {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Nombre</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted"></Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh", // Ajusta el tamaño para centrar verticalmente
+        backgroundColor: "#f8f9fa", // Color de fondo opcional
+        border: "1px",
+      }}
+    >
+      <Form style={{ width: "300px" }}>
+        {" "}
+        {/* Ancho fijo opcional */}
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control type="text" placeholder="Ingresa tu nombre" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Correo</Form.Label>
+          <Form.Control type="email" placeholder="Ingresa tu correo" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control type="password" placeholder="Contraseña" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Recuérdame" />
+        </Form.Group>
+        <Button variant="primary" type="submit" href="/creados">
+          Enviar
+        </Button>
+      </Form>
+    </div>
   );
 };
